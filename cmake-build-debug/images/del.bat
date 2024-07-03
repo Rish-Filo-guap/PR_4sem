afe787cda1d7463933031e6de25c58063151b2bd
Rish-Filo-guap <filoliolla@gmail.com>
2024-07-03 16:48:57 +0300

    добавлены картинки

---
+++ cmake-build-debug/images/del.bat
@@ -0,0 +1,4 @@
+del /q "C:\Users\Anton\CLionProjects\PR_4sem\cmake-build-debug\images\new"
+del /q "C:\Users\Anton\CLionProjects\PR_4sem\cmake-build-debug\images\bin"
+del /q "C:\Users\Anton\CLionProjects\PR_4sem\cmake-build-debug\images\rle"
+del /q "C:\Users\Anton\CLionProjects\PR_4sem\cmake-build-debug\images\haf"
